c0fc428e16206e3c76e9c9091e4f160d0b0a1936
Brendon <brendon_gi.13@hotmail.com>
2020-05-17 19:46:02 -0400

    arquivos iniciais do projeto

---
+++ src/main/java/br/com/hellochat/HellochatApplication.java
@@ -0,0 +1,13 @@
+package br.com.hellochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellochatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellochatApplication.class, args);
+	}
+
+}
