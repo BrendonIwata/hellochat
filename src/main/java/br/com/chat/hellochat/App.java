5c0267619fb87008ded1767d2857a289525322b3
Brendon <brendon_gi.13@hotmail.com>
2020-05-22 20:53:41 -0400

    arquivos iniciais do novo projeto

---
+++ src/main/java/br/com/chat/hellochat/App.java
@@ -0,0 +1,13 @@
+package br.com.chat.hellochat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
